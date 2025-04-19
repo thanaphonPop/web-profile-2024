c246ff3ddf44704891cc876345a342d561956329
thanaphon_man <thanaphon.maneerat@gmail.com>
2025-04-19 14:57:45 +0700

    modified:   src/app/_components/Bio.tsx

--- src/app/_components/Bio.tsx
+++ src/app/_components/Bio.tsx
@@ -8,6 +8,10 @@ const Bio = (props: Props) => {
     <Card title="Bio">
       <div>
       I am a student Bachelor of Computer Engineering and Artificial Intelligence.
+      I have experience in internship at jobbkk dot com recruitment company limited for 8 months.
+      I have a roll to be a Front end developer.The main languages used are php, html and css.
+      The work I did during my internship were changing Job applicants Dashboard and Entrepreneur Dashboard, creating a hotel job search page, 
+      and creating a Treading search page for the back-end system to easily add information.
       </div>
     </Card>
   );
