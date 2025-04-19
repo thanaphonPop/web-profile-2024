0f511b927c8fed77849ad0b54eb4a3d9acdfe46d
thanaphon_man <thanaphon.maneerat@gmail.com>
2025-04-19 12:19:29 +0700

    modified:   src/app/_components/Bio.tsx

--- src/app/_components/Bio.tsx
+++ src/app/_components/Bio.tsx
@@ -7,9 +7,7 @@ const Bio = (props: Props) => {
   return (
     <Card title="Bio">
       <div>
-      I am a student Bachelor of Computer Engineering and Artificial Intelligence. I have an aptitude for 
-      doing Deep learning in CNN (Convolutional Neural Network) and machine learning .I can deployed Ai model
-      on the web and applications.Therefore has the ability to write web applications and mobile apps.
+      I am a student Bachelor of Computer Engineering and Artificial Intelligence.
       </div>
     </Card>
   );
